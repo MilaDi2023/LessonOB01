40377c06d24489ccda90778c05e26c3b7594132e
MilaDi2023 <133892507+MilaDi2023@users.noreply.github.com>
2024-04-01 02:27:04 +0300

    add new comments

--- main.py
+++ main.py
@@ -6,22 +6,22 @@ class Warrior():
         self.endurance = endurance
         self.hair_color = hair_color
 
-    def sleep(self):
+    def sleep(self): # Метод Спать
         print(f"Воин {self.name} лёт спать")
         self.endurance += 2
 
-    def eat(self):
+    def eat(self): # Метод Кушать
         print(f"Воин {self.name} решил подкрепиться")
         self.power += 1
 
-    def hit(self):
+    def hit(self): # Метод Драться
         print(f"Воин {self.name} дерётся")
         self.endurance -= 6
 
-    def walk(self):
+    def walk(self): # Метод Ходить
         print(f"Воин {self.name} решил прогуляться")
 
-    def info(self):
+    def info(self): # Метод Вывод всей инфы о воине
         print(f"Имя воина - {self.name}")
         print(f"Его сила - {self.power}")
         print(f"Его выносливость - {self.endurance}")
